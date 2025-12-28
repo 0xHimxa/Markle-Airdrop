74400451b6679c7e52cf5f69b0d773c86f42dcbf
himxa <himxa0x@gmail.com>
2025-12-28 15:32:17 +0100

    add the makemerkle to help us create our harsh of the allowed address:

--- test/merkleAirdrop.t.sol
+++ test/merkleAirdrop.t.sol
@@ -5,7 +5,7 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Test,console} from "forge-std/Test.sol";
-
+import { MerkleAirdrop} from "src/merkleAridrop.sol";
 
 contract MerKleAirdropTest is Test{
 
