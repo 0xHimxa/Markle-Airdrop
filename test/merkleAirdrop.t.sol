ad9738e0d41bd0b756321374dd498349bffc6137
himxa <himxa0x@gmail.com>
2026-01-01 23:47:49 +0100

    removed unused import and make the deploy script

--- test/merkleAirdrop.t.sol
+++ test/merkleAirdrop.t.sol
@@ -1,13 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
-
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Test,console} from "forge-std/Test.sol";
 import { MerkleAirdrop} from "src/merkleAridrop.sol";
 import {BagalToken} from "src/BagalToken.sol";
 import {Test,console} from "forge-std/Test.sol";
+import {DeployMerKleAirdropTest} from "script/DeployMerkleAdrop.s.sol";
 contract MerKleAirdropTest is Test{
 bytes32[] public PROOF;
 MerkleAirdrop public merkleAirdrop;
@@ -15,16 +12,14 @@ BagalToken public bagalToken;
 bytes32 public Root = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
 address public user;
 uint256 userKey;
-
+uint256 AMOUNT = 25 * 1e18;
     function setUp() public{
-
+DeployMerKleAirdropTest deployerMerkle = new DeployMerKleAirdropTest();
       PROOF = new bytes32[](2);
 PROOF[0] = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
 PROOF[1] = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576; 
-        bagalToken = new BagalToken();
-        merkleAirdrop = new MerkleAirdrop(Root,bagalToken);
+        (merkleAirdrop,bagalToken) = deployerMerkle.deployMerkleAirdrops();
       (user,userKey) = makeAddrAndKey("user");
-    //  bagalToken.mint(address(bagalToken), 100 ether);
    
    
     }
@@ -33,9 +28,15 @@ PROOF[1] = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
 
 function testUseranlaim() external{
    uint256 startingBalance = bagalToken.balanceOf(user);
-
+console.log(startingBalance,'User Starting Balance');
    vm.prank(user);
-   
+   merkleAirdrop.claim(user,AMOUNT,PROOF);
+
+
+
+   uint256 endingBalance = bagalToken.balanceOf(user);
+console.log(endingBalance,'User Ending Balance');
+assertEq(endingBalance-startingBalance , AMOUNT);
    
 }
 
