01170c5e982275af4fb6fa7dd79ec562ab9ec868
himxa <himxa0x@gmail.com>
2026-01-02 01:24:54 +0100

    tested;  so that friend can pay for us

--- test/merkleAirdrop.t.sol
+++ test/merkleAirdrop.t.sol
@@ -13,6 +13,7 @@ contract MerKleAirdropTest is Test {
     bytes32 public Root = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
     address public user;
     uint256 userKey;
+    address gasPayer;
     uint256 AMOUNT = 25 * 1e18;
 
     function setUp() public {
@@ -22,13 +23,19 @@ contract MerKleAirdropTest is Test {
         PROOF[1] = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
         (merkleAirdrop, bagalToken) = deployerMerkle.deployMerkleAirdrops();
         (user, userKey) = makeAddrAndKey("user");
+        gasPayer = makeAddr("gasPayer");
     }
 
     function testUseranlaim() external {
         uint256 startingBalance = bagalToken.balanceOf(user);
+        bytes32 digest = merkleAirdrop.getMessageHash(user, AMOUNT);
+
         console.log(startingBalance, "User Starting Balance");
-        vm.prank(user);
-        merkleAirdrop.claim(user, AMOUNT, PROOF);
+     
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userKey, digest);
+
+        vm.prank(gasPayer);
+        merkleAirdrop.claim(user, AMOUNT, PROOF, v, r, s);
 
         uint256 endingBalance = bagalToken.balanceOf(user);
         console.log(endingBalance, "User Ending Balance");
